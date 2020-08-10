cbaf45b3394904b07513190e71c1498d64706099
Mathews Alufwani <mathewalufwani@gmail.com>
2020-08-10 17:04:46 +0300

    Add post method

--- insta/tests.py
+++ insta/tests.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 # Create your tests here.
 class TestProfile(TestCase):
     def setUp(self):
-        self.user = User(username='barasa')
+        self.user = User(username='mat')
         self.user.save()
 
         self.profile_test = Profile(id=1, name='image', profile_picture='default.jpg', bio='i am tired',user=self.user)
@@ -21,20 +21,9 @@ class TestProfile(TestCase):
 
 class TestPost(TestCase):
     def setUp(self):
-        self.profile_test = Profile(name='barasa', user=User(username='cecibarasa'))
+        self.profile_test = Profile(name='mat', user=User(username='alufwany'))
         self.profile_test.save()
 
         self.image_test = Post(image='default.png', name='test', caption='random', user=self.profile_test)
 
-    def test_insatance(self):
-        self.assertTrue(isinstance(self.image_test, Post))
-
-    def test_save_image(self):
-        self.image_test.save_image()
-        images = Post.objects.all()
-        self.assertTrue(len(images) > 0)
-
-    def test_delete_image(self):
-        self.image_test.delete_image()
-        after = Profile.objects.all()
-        self.assertTrue(len(after) < 1)
+    
